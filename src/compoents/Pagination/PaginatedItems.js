e8f2d15ce24a33525e5c4bd4e3e501c00a7fec3b
Harsha <harsha0770@gmail.com>
2022-10-08 18:25:59 +0530

    added pagination

---
+++ src/compoents/Pagination/PaginatedItems.js
@@ -0,0 +1,47 @@
+import ProductsTable from "../ProductsTable/ProductsTable";
+import ReactPaginate from "react-paginate";
+import classes from "./pagination.module.css";
+import { useEffect, useState } from "react";
+
+function PaginatedItems(props) {
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = props.itemsPerPage;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(
+      Object.entries(props.productsData).slice(itemOffset, endOffset)
+    );
+
+    setPageCount(
+      Math.ceil(Object.keys(props.productsData).length / itemsPerPage)
+    );
+  }, [itemOffset, props.productsData, itemsPerPage]);
+
+  const handlePageClick = (event) => {
+    const newOffset =
+      (event.selected * itemsPerPage) % Object.keys(props.productsData).length;
+    setItemOffset(newOffset);
+  };
+  return (
+    <>
+      {currentItems !== null && (
+        <ProductsTable products={Object.fromEntries(currentItems)} />
+      )}
+      <div className={`${classes.pagination} text-center`}>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+        />
+      </div>
+    </>
+  );
+}
+export default PaginatedItems;
