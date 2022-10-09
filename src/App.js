11a580a2235b27d682eb6e7dc141514db9926272
Harsha <harsha0770@gmail.com>
2022-10-10 00:18:59 +0530

    all features are added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FileInputForm from "./compoents/FileInputForm/FileInputForm";
 import fileProcessor from "./compoents/FileProcessor/fileProcessor";
 import PaginatedItems from "./compoents/Pagination/PaginatedItems";
+import SearchFeild from "./compoents/SearchFeild/SearchFeild";
 
 function App() {
   const [productsData, setProductsData] = useState(null);
+  const [searchText, setSearchText] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState(null);
+
+  const onSearchTextChangeHandler = (event) => {
+    setSearchText(
+      event.target.value === "" ? null : event.target.value.toLowerCase()
+    );
+  };
 
   const onSubmitHandler = (file) => {
     fileProcessor(file).then((data) => setProductsData(data));
   };
 
+  useEffect(() => {
+    if (searchText === null) setFilteredProducts(productsData);
+    else
+      productsData !== null &&
+        setFilteredProducts(
+          Object.fromEntries(
+            Object.entries(productsData).filter((key) =>
+              key[0].toLowerCase().includes(searchText)
+            )
+          )
+        );
+  }, [productsData, searchText]);
+
   return (
     <div>
       <FileInputForm onSubmitHandler={onSubmitHandler} />
+
       {productsData !== null && (
-        <PaginatedItems productsData={productsData} itemsPerPage={20} />
+        <>
+          <SearchFeild onChange={onSearchTextChangeHandler} />
+          {filteredProducts !== null && (
+            <PaginatedItems productsData={filteredProducts} itemsPerPage={20} />
+          )}
+        </>
       )}
-      {/* {productsData !== null && <ProductsTable products={productsData} />} */}
     </div>
   );
 }
---
+++ src/compoents/SearchFeild/SearchFeild.js
@@ -0,0 +1,20 @@
+import classes from "./SearchFeild.module.css";
+import SearchIcon from "../../assets/icon30.svg";
+const SearchFeild = (props) => {
+  return (
+    <div className={`${classes.search} text-center`}>
+      <div className={classes.wrapper}>
+        <input
+          id="search"
+          type={"text"}
+          placeholder="Search here..."
+          onChange={props.onChange}
+        />
+        <label htmlFor="search">
+          <img src={SearchIcon} />
+        </label>
+      </div>
+    </div>
+  );
+};
+export default SearchFeild;
